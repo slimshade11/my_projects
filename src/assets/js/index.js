8f00793b7dbe2d381d1f719627856af4cffa37f1
Piotr Krzaczkowski <piotr.krzaczkowski@embiq.com>
2022-03-19 22:49:58 +0100

    initial search bar structure

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -2,6 +2,7 @@ const projectList = document.getElementById('project-list');
 const myProjects = [
   { name: 'Form Validator', link: './form-validator.html' },
   { name: 'Movie seat booking', link: './movie-seat-booking.html' },
+  { name: 'Search bar', link: './search-bar.html' },
 ];
 
 window.onload = () => {
