d04f987e9f92d15cb843ea8e15ae531eccb54dc2
Piotr Krzaczkowski <piotr.krzaczkowski@embiq.com>
2022-03-19 16:28:04 +0100

    add form validators

--- src/assets/js/form-validator.js
+++ src/assets/js/form-validator.js
@@ -18,12 +18,15 @@ function showSuccess(input) {
   formControl.className = 'pb-4 mb-3 form-control success';
 }
 
-function isValidEmail(email) {
-  return String(email)
-    .toLowerCase()
-    .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
+function checkEmail(input) {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not valid');
+  }
 }
 
 function checkRequired(inputArr) {
@@ -36,6 +39,22 @@ function checkRequired(inputArr) {
   });
 }
 
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(input, `${getInputFieldName(input)} must be at least ${min} caracters`);
+  } else if (input.value.length > max) {
+    showError(input, `${getInputFieldName(input)} must be less than ${max} caracters`);
+  } else {
+    showSuccess(input);
+  }
+}
+
+function checkPasswordMatch(input, input2) {
+  if (input.value !== input2.value) {
+    showError(input2, 'Passoword does not match');
+  }
+}
+
 function getInputFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
@@ -44,4 +63,8 @@ form.addEventListener('submit', function (event) {
   event.preventDefault();
 
   checkRequired(formInputs);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPasswordMatch(password, password2);
 });
