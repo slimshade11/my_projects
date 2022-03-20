b0194b3699f4310440075794bf205ff9d5888d42
Piotr Krzaczkowski <piotr.krzaczkowski@embiq.com>
2022-03-20 18:59:13 +0100

    handle progress bar

--- src/assets/js/custom-video-player.js
+++ src/assets/js/custom-video-player.js
@@ -7,7 +7,7 @@ const timestamp = document.getElementById('timestamp');
 video.addEventListener('click', toggleVideoStatus);
 video.addEventListener('pause', updatePlayButton);
 video.addEventListener('play', updatePlayButton);
-video.addEventListener('stop', updateProgress);
+video.addEventListener('timeupdate', updateProgress);
 
 play.addEventListener('click', toggleVideoStatus);
 stop.addEventListener('click', stopVideo);
@@ -30,7 +30,19 @@ function updatePlayButton() {
 }
 
 function updateProgress() {
-  return true;
+  progress.value = (video.currentTime / video.duration) * 100;
+
+  let mins = Math.floor(video.currentTime / 60);
+  if (mins < 10) {
+    mins = '0' + String(mins);
+  }
+
+  let secs = Math.floor(video.currentTime % 60);
+  if (secs < 10) {
+    secs = '0' + String(secs);
+  }
+
+  timestamp.innerHTML = `${mins}:${secs}`;
 }
 
 function stopVideo() {
@@ -39,5 +51,5 @@ function stopVideo() {
 }
 
 function setVideoProgress() {
-  return true;
+  video.currentTime = (parseInt(progress.value) * video.duration) / 100;
 }
