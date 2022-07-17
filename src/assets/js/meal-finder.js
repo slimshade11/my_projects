d0685e716040ccef99d4f5ff9a445c446b2c9a7a
Piotr Krzaczkowski <piotr.krzaczkowski@embiq.com>
2022-07-17 12:12:04 +0200

    get meal by id

--- src/assets/js/meal-finder.js
+++ src/assets/js/meal-finder.js
@@ -34,8 +34,8 @@ function searchMeal(event) {
               (meal) => `
                 <div class="pointer relative text-center w-full h-full meal">
                   <img class="w-full h-full border-round-md" src="${meal.strMealThumb}"/>
-                  <div data-mealID="${meal.idMeal}">
-                      <h3 class="h-full w-full absolute flex align-items-center justify-content-center meal-info" >
+                  <div>
+                      <h3 data-mealId="${meal.idMeal}" class="h-full w-full absolute flex align-items-center justify-content-center meal-info" >
                         ${meal.strMeal}
                       </h3>
                   </div>
@@ -50,4 +50,24 @@ function searchMeal(event) {
   } else alert('Type name of meal you are looking for');
 }
 
+function getMealById(mealId) {
+  fetch('www.themealdb.com/api/json/v1/1/lookup.php?i=' + mealId)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+  // TODO: display single meal in template
+}
+
 submit.addEventListener('click', searchMeal);
+
+mealsEl.addEventListener('click', (e) => {
+  const mealInfo = e.composedPath().find((item) => {
+    if (item.classList) {
+      return item.classList.contains('meal-info');
+    } else return false;
+  });
+
+  if (mealInfo) {
+    const mealId = mealInfo.getAttribute('data-mealId');
+    getMealById(mealId);
+  }
+});
